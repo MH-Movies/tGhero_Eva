ad3c47d9084f76d92493e8993708657192c8b3b2
Rishi <64667088+rishye@users.noreply.github.com>
2022-05-20 19:50:32 +0530

    Update ping.py

--- plugins/ping.py
+++ plugins/ping.py
@@ -10,11 +10,10 @@ STICKER_IDS = "CAACAgUAAxkBAAIBaWKGlZpP4wHeFOC1e9tNXxWUs2OCAALXBAAC9Q-oV8yJbf0Fb
 
 @Client.on_message(filters.command("ping") & filters.private)
 async def ping(bot: Client, message):
-    #await message.reply_sticker(
-        #sticker=choice(STICKER_IDS),
-    #)
     start_ts=time.time()
-    beep = await message.reply("...")
+    beep = await message.reply_text(".")
+    sheep=await beep.edit("..")
+    ded=await sheep.edit("...")
     end_ts=time.time()
     time_taken=(end_ts - start_ts) * 1000
     await beep.edit(f"Ping : {time_taken}")
