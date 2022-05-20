b7a51bbe7ff90a3a9b53ad8a74b2a646da400630
Rishi <64667088+rishye@users.noreply.github.com>
2022-05-20 12:23:17 +0530

    Create ping.py

---
+++ plugins/ping.py
@@ -0,0 +1,21 @@
+import time
+from random import chocie
+from pyrogram import Client, filters
+
+STICKER_IDS = "CAACAgUAAxkBAAIBaWKGlZpP4wHeFOC1e9tNXxWUs2OCAALXBAAC9Q-oV8yJbf0Fb9C9JAQ",
+              "CAACAgUAAxkBAAIBamKGlZoWkaI8sLTTBi0x3PbNAjUPAALRBgAC-7OoV_DwW_xcFC5fJAQ",
+              "CAACAgUAAxkBAAIBa2KGlZoExzuAnEGjhEqEZw71xVmmAAKmBQACpSmoV8ufoK__mXe1JAQ",
+              "CAACAgUAAxkBAAIBbGKGlZr4iuzVGWkwu1sy431DcKYCAAJqBAAC7nuxVzQ8Wb80s0UtJAQ"
+
+
+@Client.on_message(filters.command("ping") & filters.private)
+async def ping(bot: Client, message):
+    await message.delete("/")
+    await message.reply_sticker(
+        sticker=choice(STICKER_IDS),
+    )
+    start_ts=time.time()
+    beep = await message.reply("...")
+    end_ts=time.time()
+    time_taken=(end_ts - start_ts) * 1000
+    await beep.edit(f"Ping : {time_taken}")
